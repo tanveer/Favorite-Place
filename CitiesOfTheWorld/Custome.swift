b19e446285cdb9154e381e8817171af3e6dc8d47
tmbashir <ifarting@gmail.com>
2015-12-25 16:28:34 -0500

    add EasyAnimation and gitignore

--- CitiesOfTheWorld/Custome.swift
+++ CitiesOfTheWorld/Custome.swift
@@ -13,6 +13,7 @@ import UIKit
 //constant
 let SHADOW_COLOR: CGFloat = 100.0 / 255.0
 let VIEW_BACKGROUND = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1)
+
 let citiesAndCounteries = [ "Alhambra" : "Spain", "Amsterdam" : "Netherlands", "Angkor_Siem":"Cambodia", "Bangkok" : "Thailand","Barcelona":"Spain", "Burj_Khalifa":"Dubai", "Charles_Bridge":"Prague", "Chichen_Itza":"Mexico", "Eiffle_Tower":"France", "Great_Wall":"China", "Hong_Kong":"Hong_Kong", "Istanbul":"Turkey", "Kuala_Lampur":"Malasyia", "London":"UK", "Big_Ben":"London", "Machu_Picchu":"Peru", "Marina":"Dubai", "Milan_Cathedral":"Italy", "Milan":"Itlay", "New_York":"USA", "Notre_Dame":"France", "Opera_House":"Sidney","Shanghai":"China", "Singapore":"Singapore", "Seoul":"South Korea", "St_Petersburg":"Russia","Taj_Mahal":"India", "Tokyo":"Japan", "Vienna":"Italy"]
 
 let IMAGES_OF_CITIES = ["Japan":["Tokyo", "Kyoto", "Kamakura", "Osaka", "Yokohama"], "USA":["New York", "Chicago","Los Angelos", "San Fransisco", "Boston", "Philadelphia"], "UK":["London", "Manchester", "Wales"]]
--- CitiesOfTheWorld/DetailViewController.swift
+++ CitiesOfTheWorld/DetailViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class DetailViewController: UIViewController {
     @IBOutlet weak var frameView: UIView!
-    @IBOutlet weak var backView: UIView!
+    //@IBOutlet weak var backView: UIView!
     @IBOutlet weak var cityImage:UIImageView!
     @IBOutlet weak var cityName:UILabel!
     var city:String = ""
@@ -20,32 +20,40 @@ class DetailViewController: UIViewController {
     var count:Int = 0
     var screenSize:CGRect?
     var timer = NSTimer()
+    var num = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        frameView.setShadow(frameView)
         //backView.setShadow(backView)
         screenSize = UIScreen.mainScreen().bounds
-        frameView.setShadow(frameView)
         cities = IMAGES_OF_CITIES[counteryName]!
-        timer = NSTimer.scheduledTimerWithTimeInterval(3.0, target: self, selector: Selector("animation"), userInfo: nil, repeats: true)
+        num = cities.count
     }
-
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        cityName.text = "\(citiesAndCounteries[cities[count]]!)"
+        timer = NSTimer.scheduledTimerWithTimeInterval(1.5, target: self, selector: Selector("animation"), userInfo: nil, repeats: true)
+    }
+    
     //Carousel animation
-
     func animation(){
-        let num = cities.count
-        if count < num {
-            UIView.animateWithDuration(0.5, delay: 1.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 0, options:
-                UIViewAnimationOptions.CurveEaseIn, animations: {
-                    self.cityName.text = "\(self.cities[self.count])"
-                    self.cityImage.image = UIImage(named: "\(self.cities[self.count]).jpg" )
-//                    self.frameView.center = CGPointMake(self.frameView.center.x - (self.screenSize!.width), self.frameView.center.y)
+        if count < num{
+            UIView.animateWithDuration(1.5, delay: 0.5, usingSpringWithDamping:0.5, initialSpringVelocity: 3.0, options:
+                UIViewAnimationOptions.CurveLinear, animations: {
+                self.frameView.center = CGPointMake(self.frameView.center.x - (self.screenSize!.width), self.frameView.center.y)
+                self.cityImage.image = UIImage(named: "\(self.cities[self.count]).jpg")
                 }, completion:{ complete in
-//                    if complete {
-//                        UIView.animateWithDuration(0, delay: 1.0, options: .CurveLinear, animations: {
-//                        self.frameView.center = CGPointMake(self.frameView.center.x + (self.screenSize!.width), self.frameView.center.y)
-//                        }, completion:nil)
-//                    }
+                    if complete {
+                        UIView.animateWithDuration(1.0, animations: {
+                            self.frameView.center = CGPointMake(self.frameView.center.x, self.frameView.center.y - 300)
+                        })
+                    }
             })
             ++count
         } else {
@@ -53,29 +61,16 @@ class DetailViewController: UIViewController {
         }
     }
     
-    //testing
     
-//    func animation(){
-//        let num = Int(cities.count)
-//        if count < num {
-//            UIView.animateWithDuration(0.5, delay: 0, usingSpringWithDamping: 1.0, initialSpringVelocity: 0, options: UIViewAnimationOptions.CurveEaseIn, animations: {
-//                self.cityImage.center = CGPointMake(self.cityImage.center.x - (self.screenSize?.width)!, self.cityImage.center.y)
-//                self.cityImage.image = UIImage(named: self.cities[self.count])
-//                self.cityImage.alpha = 1
-//                }, completion:nil)
-//            ++count
-//        } else {
-//            timer.invalidate()
-//            count = 0
-//        }
-//    }
-
-    
-    
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
-        cityImage.image = UIImage(named: "\(imageName)")
-        cityName.text = city
-        cityImage.center = CGPointMake(cityImage.center.x - backView.frame.width, cityImage.center.y)
+    func setupView(){
+        if count < num{
+            UIView.animateWithDuration(2.0, delay:0, options: .CurveLinear, animations: {
+                self.frameView.center = CGPointMake(self.frameView.center.x - (self.screenSize!.width), self.frameView.center.y)
+                self.cityImage.image = UIImage(named: "\(self.cities[self.count]).jpg")
+                }, completion: nil)
+            count++
+        } else{
+            count = 0
+        }
     }
 }
